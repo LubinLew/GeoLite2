ff466806e2ff527d79fc53679c2938ae9ab44feb
Lubin <lgbxyz@gmail.com>
2023-05-15 09:42:43 +0800

    Update build.sh

--- release/Docker/build.sh
+++ release/Docker/build.sh
@@ -2,9 +2,8 @@
 set -ex
 cd `dirname $0`
 ##########################################################
+
 IMAGE="ghcr.io/lubinlew/geolite2_builder:latest"
 
-if docker pull ${IMAGE} &> /dev/null ; then
-  docker build -t ${IMAGE} .
-  docker push ${IMAGE}
-fi
+docker build -t ${IMAGE} .
+
